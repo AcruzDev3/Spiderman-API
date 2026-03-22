1ea8139a0008b80d6d9cb3478b0f6d9aec2ec972
AcruzDev3 <acruzdev03@gmail.com>
2026-03-22 03:09:46 +0100

    refactor(dependency injection) : add class for dependency injection

---
+++ LIB/Extensions/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using LIB.Interfaces.IManagers;
+using LIB.Interfaces.IRepositories;
+using LIB.Managers;
+using LIB.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LIB.Extensions
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services) {
+            services.AddScoped<ICrimeManager, CrimeManager>();
+            services.AddScoped<ICriminalManager, CriminalManager>();
+            services.AddScoped<IUserManager, UserManager>();
+            services.AddScoped<IAddressManager, AddressManager>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services) {
+            services.AddScoped<ICrimeRepository, CrimeRepository>();
+            services.AddScoped<ICriminalRepository, CriminalRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IAddressRepository, AddressRepository>();
+
+            return services;
+        }
+    }
+}
