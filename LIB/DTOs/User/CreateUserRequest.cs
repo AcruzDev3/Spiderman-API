67974cad2cbe13c3c0f97b1be4ad186ad9713e6c
AcruzDev3 <acruzdev03@gmail.com>
2026-03-22 03:10:21 +0100

    refactor(dtos) : modify users request

--- LIB/DTOs/User/CreateUserRequest.cs
+++ LIB/DTOs/User/CreateUserRequest.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LIB.DTOs.User
+﻿namespace LIB.DTOs.User
 {
     public class CreateUserRequest
     {
