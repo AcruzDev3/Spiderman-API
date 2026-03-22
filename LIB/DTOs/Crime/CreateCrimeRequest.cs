a515f57982480efe03fb3d6e6c82710256e7caa7
AcruzDev3 <acruzdev03@gmail.com>
2026-03-22 02:57:19 +0100

    refactor(DTOs) :  add List Users

--- LIB/DTOs/Crime/CreateCrimeRequest.cs
+++ LIB/DTOs/Crime/CreateCrimeRequest.cs
@@ -12,6 +12,7 @@ namespace LIB.DTOs.Crime
         public string? TypeName { get; set; }
         public string? Description { get; set; }
         public int AddressId { get; set; }
-        public List<int>? CriminalsIds { get; set; }
+        public List<int>? CriminalIds { get; set; }
+        public List<int>? UserIds { get; set; }
     }
 }
