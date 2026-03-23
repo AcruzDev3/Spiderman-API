5605ed4acc0e545a7f5568f72c6f118e30a36778
AcruzDev3 <acruzdev03@gmail.com>
2026-03-23 22:04:09 +0100

    feat(dto) : add dto UpdateUserPasswordRequest

---
+++ API/DTOs/User/UpdateUserPasswordRequest.cs
@@ -0,0 +1,20 @@
+﻿using LIB.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs.User
+{
+    public class UpdateUserPasswordRequest
+    {
+        [Required(ErrorMessage = "La contraseña anterior es obligatoria")]
+        [StrongPasswrod]
+        public string previousPassword { get; set; }
+
+        [Required(ErrorMessage = "La nueva contraseña es obligatoria")]
+        [StrongPasswrod]
+        public string newPassword { get; set; }
+
+        [Required(ErrorMessage = "La confirmación de la nueva contraseña es obligatoria")]
+        [StrongPasswrod]
+        public string newPasswordConfirmation { get; set; }
+    }
+}
