8a4282ee32b686660713c4d215500414567bc686
AcruzDev3 <acruzdev03@gmail.com>
2026-03-23 19:45:16 +0100

    feat(Enums) : add controller JsonStringEnumConverter

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,14 @@
 using LIB.Extensions;
 using LIB.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options => {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
