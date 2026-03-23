067e837832810bd56a162bc977732d68f24d9150
AcruzDev3 <acruzdev03@gmail.com>
2026-03-23 19:44:34 +0100

    feat(Security) : add Hasher, Interface and Class Password

---
+++ LIB/Models/Password.cs
@@ -0,0 +1,23 @@
+﻿using LIB.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIB.Models
+{
+    public class Password
+    {
+        public string Value { get; }
+
+        private Password(string value) {
+            Value = value;
+        }
+
+        public static Password Create(string rawPasswordm, IPasswordHasher hasher) {
+            string hashed = hasher.Hash(rawPasswordm);
+            return new Password(hashed);
+        }
+    }
+}
---
+++ LIB/Security/BCryptPasswordHasher.cs
@@ -0,0 +1,19 @@
+﻿using LIB.Interfaces;
+using LIB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIB.Security
+{
+    public class BCryptPasswordHasher : IPasswordHasher
+    {
+        public string Hash(string password)
+            => BCrypt.Net.BCrypt.HashPassword(password);
+
+        public bool Verify(string hashedPassword, string providePasssword)
+            => BCrypt.Net.BCrypt.Verify(providePasssword, hashedPassword);
+    }
+}
