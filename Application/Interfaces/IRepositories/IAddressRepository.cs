227dca8ef35fd191300c7855fc7a74193b3b2c97
AcruzDev3 <acruzdev03@gmail.com>
2026-03-26 20:59:31 +0100

    refactor(interfaces) : move repositories interfaces to application project

---
+++ Application/Interfaces/IRepositories/IAddressRepository.cs
@@ -0,0 +1,16 @@
+﻿using LIB.Models;
+using LIB.ViewModels;
+
+namespace Application.Interfaces.IRepositories
+{
+    public interface IAddressRepository
+    {
+        Task<Address?> GetById(int id);
+        Task<List<Address>?> GetAll();
+        Task<Address?> Exists(AddressViewModel address);
+        Task Add(Address address);
+        void Update(Address address);
+        void Delete(Address address);
+        Task<int> SaveChanges();
+    }
+}
---
+++ Application/Interfaces/IRepositories/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Application.Interfaces.IRepositories
+{
+    public interface IUserRepository
+    {
+        Task<UserEntity?> GetById(int id); 
+        Task<List<User>?> GetByIds(List<int> ids);
+        Task<List<User>> GetAll();
+        Task<List<Crime>?> GetCrimes(User model);
+        Task<User?> Exists(UserViewModel viewModel);
+        Task Add(User model);
+        void Update(User model);
+        void Delete(User model);
+        Task<int> SaveChanges();
+        Task<Role?> GetRoleAsync(string? role);
+    }
+}
