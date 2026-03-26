8334de600f263154f8c40cfbb3f735377dc169b3
AcruzDev3 <acruzdev03@gmail.com>
2026-03-26 21:00:32 +0100

    refactor(extensions) : add dependency injection of application project

---
+++ Application/Extensions/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Application.Interfaces.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Application.Services;
+
+namespace LIB.Extensions
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services) {
+            services.AddScoped<ICrimeService, CrimeService>();
+            services.AddScoped<ICriminalService, CriminalService>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IAddressService, AddressService>();
+
+            return services;
+        }
+    }
+}
