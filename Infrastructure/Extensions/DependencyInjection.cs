88ab0b9979241dbd8b4764d985a9f3a64ca77f9a
AcruzDev3 <acruzdev03@gmail.com>
2026-03-26 20:54:57 +0100

    refactor(extensions) : add dependency injection

---
+++ Infrastructure/Extensions/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Application.Interfaces;
+using Application.Interfaces.IRepositories;
+using Infrastructure.Repositories;
+using Infrastructure.Security;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Extensions
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services) {
+            services.AddScoped<ICrimeRepository, CrimeRepository>();
+            services.AddScoped<ICriminalRepository, CriminalRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IAddressRepository, AddressRepository>();
+            services.AddScoped<IPasswordHasher, BCryptPasswordHasher>();
+
+            return services;
+        }
+    }
+}
