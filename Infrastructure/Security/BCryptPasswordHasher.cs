4b23f531cc2e91985c202391e91d65084c121f68
AcruzDev3 <acruzdev03@gmail.com>
2026-03-26 20:55:57 +0100

    refactor(security) : move security to infrastructure project

---
+++ Infrastructure/Security/BCryptPasswordHasher.cs
@@ -0,0 +1,13 @@
+﻿using Application.Interfaces;
+
+namespace Infrastructure.Security
+{
+    public class BCryptPasswordHasher : IPasswordHasher
+    {
+        public string Hash(string password)
+            => BCrypt.Net.BCrypt.HashPassword(password);
+
+        public bool Verify(string hashedPassword, string providePasssword)
+            => BCrypt.Net.BCrypt.Verify(providePasssword, hashedPassword);
+    }
+}
