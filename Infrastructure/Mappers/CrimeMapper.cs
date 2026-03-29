3898b695692702e346d73efd6901b60e4de0036d
AcruzDev3 <acruzdev03@gmail.com>
2026-03-29 20:53:53 +0200

    fix(crime mapper) : add heroes and criminals in ToEntity

--- Infrastructure/Mappers/CrimeMapper.cs
+++ Infrastructure/Mappers/CrimeMapper.cs
@@ -1,18 +1,21 @@
-﻿using Domain.Models;
+﻿using Application.Services;
+using Domain.Models;
 using Infrastructure.EF_Entities;
 
 namespace Infrastructure.Mappers
 {
     public static class CrimeMapper
     {
-        public static Crime ToDomain(CrimeEntity entity, Address address, CrimeGrade grade, CrimeType type) {
-            return new Crime(entity.CrimeId, address, grade, type, entity.Description, entity.Status, entity.DateStart, entity.DateEnd);
+        public static Crime ToDomain(CrimeEntity entity, List<User> users, List<Criminal> criminals, Address address, CrimeGrade grade, CrimeType type) {
+            return new Crime(entity.CrimeId, address, users, criminals, grade, type, entity.Description, entity.Status, entity.DateStart, entity.DateEnd);
         }
 
-        public static CrimeEntity ToEntity(Crime crime) {
+        public static CrimeEntity ToEntity(Crime crime, List<UserEntity> usersEntities, List<CriminalEntity> criminalsEntities) {
             return new CrimeEntity {
                 CrimeId = crime.Id,
                 AddressId = crime.Address.Id,
+                Heroes = usersEntities,
+                Criminals = criminalsEntities,
                 GradeId = crime.Grade.Id,
                 TypeId = crime.Type.Id,
                 Description = crime.Description,
