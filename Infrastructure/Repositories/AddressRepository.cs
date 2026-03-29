d60d884763d3056a9b22ffbea9b2ba8a184fbb51
AcruzDev3 <acruzdev03@gmail.com>
2026-03-29 19:58:45 +0200

    fix(address repository) : modify to return domain models

--- Infrastructure/Repositories/AddressRepository.cs
+++ Infrastructure/Repositories/AddressRepository.cs
@@ -1,7 +1,8 @@
 ﻿
-using Application.Interfaces.IRepositories;
+using Domain.Interfaces.IRepositories;
 using Domain.Models;
 using Infrastructure.EF_Entities;
+using Infrastructure.Mappers;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Repositories
@@ -13,42 +14,48 @@ namespace Infrastructure.Repositories
             this._context = context;
         }
 
-        public async Task<AddressEntity?> GetById(int id) {
-            return await this._context.Addresses
+        public async Task<Address?> GetById(int id) {
+            AddressEntity? entity = await this._context.Addresses
                 .AsNoTracking()
                 .FirstOrDefaultAsync(a => a.AddressId == id);
+            return AddressMapper.ToDomain(entity);
         }
 
-        public async Task<List<AddressEntity>?> GetAll() {
-            return await this._context.Addresses
-                .ToListAsync();
+        public async Task<List<Address>?> GetAll() {
+            List<AddressEntity> entities = await this._context.Addresses.ToListAsync();
+            List<Address> addresses = new List<Address>();
+            foreach(AddressEntity entity in entities)
+                addresses.Add(AddressMapper.ToDomain(entity));
+            return addresses;
         }
 
         public async Task<Address?> Exists(Address viewModel) {
             try {
                 if (viewModel == null) throw new Exception("El modelo vista de la dirección no es válido");
 
-                return await this._context.Addresses
+                AddressEntity? entity = await this._context.Addresses
                    .FirstOrDefaultAsync(m => m.Street.Equals(viewModel.Street, StringComparison.OrdinalIgnoreCase) &&
                    m.ZipCode.Equals(viewModel.ZipCode, StringComparison.OrdinalIgnoreCase) &&
                    m.Side.Equals(viewModel.Side.ToString(), StringComparison.OrdinalIgnoreCase) &&
                    m.Number == viewModel.Number
-                   );
+                );
+
+                return AddressMapper.ToDomain(entity);
             } catch (Exception) {
                 throw;
             }
         }
 
-        public async Task Add(AddressEntity address) {
-            await this._context.Addresses.AddAsync(address);
+        public async Task Add(Address address) {
+            await this._context.Addresses.AddAsync(AddressMapper.ToEntity(address));
         }
 
-        public void Update(AddressEntity address) {
-            this._context.Addresses.Update(address);
+        public void Update(Address address) {
+            this._context.Addresses.Update(AddressMapper.ToEntity(address));
         }
 
-        public void Delete(AddressEntity address) {
-            this._context.Addresses.Remove(address);
+        public void Delete(Address address) {
+            this._context.Addresses.Remove(AddressMapper.ToEntity(address));
         }
 
         public async Task<int> SaveChanges() {
