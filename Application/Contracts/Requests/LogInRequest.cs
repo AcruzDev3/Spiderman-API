ae542a096ee50b70253343435594c2ea41c713e5
AcruzDev3 <acruzdev03@gmail.com>
2026-03-31 11:57:03 +0200

    fix(request) : add email length validation

--- Application/Contracts/Requests/LogInRequest.cs
+++ Application/Contracts/Requests/LogInRequest.cs
@@ -5,7 +5,7 @@ namespace Application.Contracts.Requests {
 
         [Required(ErrorMessage = "El correo electrónico es obligatorio")]
         [EmailAddress(ErrorMessage = "El formato del correo electrónico no es válido")]
-        [StringLength(255, ErrorMessage = "La longitud del correo electrónico no pueder ser mayor a 255 caracteres")]]
+        [StringLength(255, ErrorMessage = "La longitud del correo electrónico no pueder ser mayor a 255 caracteres")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "La contraseña es obligatoria")]
