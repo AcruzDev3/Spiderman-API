39d53a080c625ee63e78569b1ed904c9ddc9ff4a
AcruzDev3 <acruzdev03@gmail.com>
2026-04-19 19:52:16 +0200

    feat(constans): add defaultimagespath

---
+++ Application/Constans/DefaultImagesPath.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Constans
+{
+    public static class DefaultImagesPath
+    {
+        public const string User = "default/user.webp";
+        public const string Criminal = "default/criminal.webp";
+        public const string Role = "default/role.webp";
+    }
+}
