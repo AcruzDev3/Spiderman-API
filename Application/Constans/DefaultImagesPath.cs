43141cf8796fff4c687befc5ffd93e8953625fe9
AcruzDev3 <acruzdev03@gmail.com>
2026-04-20 20:40:12 +0200

    fix(constans): remove Role and add Address

--- Application/Constans/DefaultImagesPath.cs
+++ Application/Constans/DefaultImagesPath.cs
@@ -4,6 +4,6 @@
     {
         public const string User = "default/user.webp";
         public const string Criminal = "default/criminal.webp";
-        public const string Role = "default/role.webp";
+        public const string Address = "default/address.webp";
     }
 }
