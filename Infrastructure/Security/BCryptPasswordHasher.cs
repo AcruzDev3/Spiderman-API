658d76ee11db69fce70b8956d1ed4448f4b68175
AcruzDev3 <acruzdev03@gmail.com>
2026-04-20 18:48:31 +0200

    feat(service): add azure service to upload images

--- Infrastructure/Security/BCryptPasswordHasher.cs
+++ Infrastructure/Security/BCryptPasswordHasher.cs
@@ -4,10 +4,18 @@ namespace Infrastructure.Security
 {
     public class BCryptPasswordHasher : IPasswordHasher
     {
-        public string Hash(string password)
-            => BCrypt.Net.BCrypt.HashPassword(password);
+        public string Hash(string password) {
+            if (String.IsNullOrEmpty(password)) 
+                throw new ArgumentNullException("La contraseña es nula o vacia");
 
-        public bool Verify(string hashedPassword, string providePasssword)
-            => BCrypt.Net.BCrypt.Verify(providePasssword, hashedPassword);
+             return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+        
+        public bool Verify(string hashedPassword, string providePasssword) {
+            if (String.IsNullOrEmpty(hashedPassword) || String.IsNullOrEmpty(providePasssword))
+                return false;
+
+            return BCrypt.Net.BCrypt.Verify(providePasssword, hashedPassword);
+        }
     }
 }
