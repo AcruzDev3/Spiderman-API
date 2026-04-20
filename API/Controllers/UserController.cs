c9a166f0cd5d8b6d5caff56814c6f28d48ac1bf5
AcruzDev3 <acruzdev03@gmail.com>
2026-04-20 18:54:06 +0200

    feat(controller): add changepassword

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -13,40 +13,40 @@ namespace API.Controllers
     public class UserController : Controller
     {
         private readonly IUserService _userService;
-        private readonly IAzureImageService _azureImageService;
-        public UserController(IUserService userService, IAzureImageService azureService) {
+        public UserController(IUserService userService) {
             this._userService = userService;
-            this._azureImageService = azureService;
         }
 
         [HttpGet("{id}")]
-        public async Task<IActionResult> GetById(int id) {
-            UserResponse viewModel = await this._userService.GetById(id);
-            return Ok(viewModel);
-        }
+        public async Task<IActionResult> GetById(int id)
+            => Ok(await this._userService.GetById(id));
+        
 
         [HttpGet]
-        public async Task<IActionResult> Index() {
-            List<UserResponse> viewModels = await this._userService.GetAll();
-            return Ok(viewModels);
-        }
+        public async Task<IActionResult> Index()
+            => Ok(await this._userService.GetAll());
+        
 
         [HttpPost]
         public async Task<IActionResult> Create(CreateUserRequest request) {
-            UserResponse user = null;
+            UserResponse userResponse = await this._userService.Create(request);
+            return CreatedAtAction(nameof(GetById), new { id = userResponse.Id}, userResponse);
+        }
+            
+        [HttpPut]
+        public async Task<IActionResult> Update(UpdateUserRequest request)
+            => Ok(await this._userService.Update(request));
 
-            if(request.Image != null) {
-                string urlImage = await this._azureImageService.UploadImageAsync(
-                    request.Image.OpenReadStream(),
-                    FolderImageEnum.Users.ToString().ToLower(),
-                    request.Image.ContentType
-                );
-                user = await this._userService.Create(request, urlImage);
-            } else {
-                user = await this._userService.Create(request, DefaultImagesPath.User);
-            }
+        [HttpPut("ChangePassword")]
+        public async Task<IActionResult> ChangePassword(ChangePasswordRequest request) {
+            await this._userService.ChangePassword(request);
+            return NoContent();
+        }
 
-            return CreatedAtAction(nameof(GetById), user);
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id) {
+            await this._userService.Delete(id);
+            return NoContent();
         }
     }
 }
