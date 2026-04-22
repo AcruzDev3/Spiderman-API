62d4b20066b9d0e1c3a65739ff4db703b6f89564
AcruzDev3 <acruzdev03@gmail.com>
2026-04-22 22:15:41 +0200

    feat(Interface): delete GetCrimes and GetCriminalRiskLevelAsyn

--- Domain/Interfaces/IRepositories/ICriminalRepository.cs
+++ Domain/Interfaces/IRepositories/ICriminalRepository.cs
@@ -7,8 +7,6 @@ namespace Domain.Interfaces.IRepositories
         Task<Criminal?> GetById(int id);
         Task<List<Criminal>?> GetByIds(List<int> ids);
         Task<List<Criminal>?> GetAll();
-        Task<List<Crime>?> GetCrimes(Criminal model);
-        Task<CriminalRiskLevel?> GetCriminalRiskLevelAsync(int RiskId);
         Task<bool> Exists(string name);
         Task<Criminal> Add(Criminal criminal);
         Task<Criminal> Update(Criminal criminal);
