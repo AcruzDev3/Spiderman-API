1095f9c5452607372c2c58cc8a152cfe1d8bc94e
AcruzDev3 <acruzdev03@gmail.com>
2026-04-25 18:40:25 +0200

    fix(auth controller): modify to async functions

---
+++ API/Controllers/V1/AuthController.cs
@@ -0,0 +1,23 @@
+﻿using Application.Contracts.Requests.Auth;
+using Application.Interfaces.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.V1
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService) {
+            this._authService = authService;
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginRequest request)
+            => Ok(await this._authService.Login(request));
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterRequest request)
+            => Ok(await this._authService.Register(request));
+    }
+}
