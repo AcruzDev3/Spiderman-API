5b8f23eca6f61b262086c0f88758915965a5bb50
AcruzDev3 <acruzdev03@gmail.com>
2026-05-16 22:29:46 +0200

    refactor(context) : delete the configurations

---
+++ Infrastructure/Persistence/SpidermanContext.cs
@@ -0,0 +1,30 @@
+﻿using Infrastructure.Persistence.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.EF_Entities;
+
+public partial class SpidermanContext : DbContext
+{
+    public SpidermanContext(DbContextOptions<SpidermanContext> options)
+        : base(options) { }
+
+    public virtual DbSet<AddressEntity> Addresses { get; set; }
+
+    public virtual DbSet<CrimeEntity> Crimes { get; set; }
+
+    public virtual DbSet<CrimeGradeEntity> CrimeGrades { get; set; }
+
+    public virtual DbSet<CrimeTypeEntity> CrimeTypes { get; set; }
+
+    public virtual DbSet<CriminalEntity> Criminals { get; set; }
+
+    public virtual DbSet<CriminalRiskLevelEntity> CriminalRiskLevels { get; set; }
+
+    public virtual DbSet<RoleEntity> Roles { get; set; }
+
+    public virtual DbSet<UserEntity> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(SpidermanContext).Assembly);
+    }
+}
